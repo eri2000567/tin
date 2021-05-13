6913b4a82bf913837d422f89d3ffdcce441dff3b
eri2000567 <78245803+eri2000567@users.noreply.github.com>
2021-05-13 21:57:50 +0900

    Update tin.user.js

--- tin.user.js
+++ tin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         match
 // @namespace    https://github.com/eri2000567/tin
-// @version      0.1
+// @version      1.0.0
 // @description  tinder match
 // @author       You
 // @match        https://tinder.com/app/recs
